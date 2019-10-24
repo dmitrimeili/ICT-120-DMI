3d5e51b3316a7bc9ba0bba8c320167e6aeca7f6a
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-10-24 14:43:51 +0200

    avancéé dans le site

--- site/JS/java.js
+++ site/JS/java.js
@@ -4,36 +4,70 @@ function init() {
     btHebergement.addEventListener("click", btHebergementClick);
     btTransport.addEventListener("click", btTransportClick);
     btActivite.addEventListener("click", btActiviteClick);
-    btParticipant.addEventListener("click",btParticipantClick);
+    btParticipant.addEventListener("click", btParticipantClick);
+    btConfRes.addEventListener("click",btConfResClick);
+    lstChambre.addEventListener("click",lstChambreClick);
+    btChecklist.addEventListener("click",btChecklistClick)
 
+    divHebergement.style.display = "none";
+    divTransport.style.display = "none";
+    divActivite.style.display = "none";
+    divParticipant.style.display = "none";
+    divChecklist.style.display="none";
 
 }
 
 function btHebergementClick() {
-    divHebergement.style.visibility='visible';
-    divTransport.style.visibility='hidden';
-    divActivite.style.visibility='hidden';
-    divParticipant.style.visibility='hidden';
+    divHebergement.style.display = "inline";
+    divTransport.style.display = "none";
+    divActivite.style.display = "none";
+    divParticipant.style.display = "none";
+
+    divMail.style.display = "none";
 }
 
-function btTransportClick(){
-    divHebergement.style.visibility='hidden';
-    divTransport.style.visibility='visible';
-    divActivite.style.visibility='hidden';
-    divParticipant.style.visibility='hidden';
+function btTransportClick() {
+    divHebergement.style.display = "none";
+    divTransport.style.display = "inline";
+    divActivite.style.display = "none";
+    divParticipant.style.display = "none";
 }
 
-function btActiviteClick(){
-    divHebergement.style.visibility='hidden';
-    divTransport.style.visibility='hidden';
-    divActivite.style.visibility='visible';
-    divParticipant.style.visibility='hidden';
+function btActiviteClick() {
+    divHebergement.style.display = "none";
+    divTransport.style.display = "none";
+    divActivite.style.display = "inline";
+    divParticipant.style.display = "none";
 }
+
 function btParticipantClick() {
-    divHebergement.style.visibility='hidden';
-    divTransport.style.visibility='hidden';
-    divActivite.style.visibility='hidden';
-    divParticipant.style.visibility='visible';
+    divHebergement.style.display = "none";
+    divTransport.style.display = "none";
+    divActivite.style.display = "none";
+    divParticipant.style.display = "inline";
 
 }
 
+function btConfResClick() {
+    if(divMail.style.display==="none"){
+        divMail.style.display="inline";
+    }
+    else{
+        divMail.style.display="none"
+    }
+
+
+}
+function lstChambreClick() {
+
+nbOccMax="5";
+}
+
+function btChecklistClick() {
+    if(divChecklist.style.display==="none"){
+        divChecklist.style.display="inline";
+    }
+    else{
+        divChecklist.style.display="none"
+    }
+}
