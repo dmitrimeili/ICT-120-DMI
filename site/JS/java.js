f4dc013609317e8eccd86e60ec95d798c7f19f0e
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-10-28 15:57:49 +0100

    travaille site

--- site/JS/java.js
+++ site/JS/java.js
@@ -5,15 +5,16 @@ function init() {
     btTransport.addEventListener("click", btTransportClick);
     btActivite.addEventListener("click", btActiviteClick);
     btParticipant.addEventListener("click", btParticipantClick);
-    btConfRes.addEventListener("click",btConfResClick);
-    lstChambre.addEventListener("click",lstChambreClick);
-    btChecklist.addEventListener("click",btChecklistClick)
+    btConfRes.addEventListener("click", btConfResClick);
+    lstChambre.addEventListener("change", lstChambreChange);
+    btChecklist.addEventListener("click", btChecklistClick);
+    btAjouter.addEventListener("click", btAjouterClick)
 
     divHebergement.style.display = "none";
     divTransport.style.display = "none";
     divActivite.style.display = "none";
     divParticipant.style.display = "none";
-    divChecklist.style.display="none";
+    divChecklist.style.display = "none";
 
 }
 
@@ -49,25 +50,51 @@ function btParticipantClick() {
 }
 
 function btConfResClick() {
-    if(divMail.style.display==="none"){
-        divMail.style.display="inline";
-    }
-    else{
-        divMail.style.display="none"
+    if (divMail.style.display === "none") {
+        divMail.style.display = "inline";
+    } else {
+        divMail.style.display = "none"
     }
 
 
 }
-function lstChambreClick() {
 
-nbOccMax="5";
+function lstChambreChange() {
+    switch (lstChambre.value) {
+        case "1":
+            nbOccMax.innerHTML = "5";
+            nbOccPrev.innerHTML = "4";
+            NumChambre.innerHTML = "1";
+            break;
+        case "2":
+            nbOccMax.innerHTML = "4";
+            nbOccPrev.innerHTML = "4";
+            NumChambre.innerHTML = "2";
+            break;
+        case "3":
+            nbOccMax.innerHTML = "6";
+            nbOccPrev.innerHTML = "6";
+            NumChambre.innerHTML = "3";
+            break;
+
+    }
 }
 
 function btChecklistClick() {
-    if(divChecklist.style.display==="none"){
-        divChecklist.style.display="inline";
+    if (divChecklist.style.display === "none") {
+        divChecklist.style.display = "inline";
+    } else {
+        divChecklist.style.display = "none";
     }
-    else{
-        divChecklist.style.display="none"
+}
+
+function btAjouterClick() {
+
+    if (txtName.value === "") {
+        alert("veuillez insérer un prénom");
+    } else {
+        Eleve = document.createElement("li");
+        Eleve.innerText = txtName.value;
+        ListeEleve.appendChild(Eleve);
     }
 }
